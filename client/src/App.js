dd0e7b1ad1755f3571c2443e3ee8ab49fd4f7eff
Samyak Maharjan <samyakjyapu33127@gmail.com>
2021-05-04 21:40:57 +0545

    Prepare client and servers

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">WebRTC app coming soon.</header>
+    </div>
+  );
+}
+
+export default App;
---
+++ server.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+const path = require("path");
+const express = require("express");
+const http = require("http");
+const app = express();
+const server = http.createServer(app);
+
+app.get("/", (req, res) => {
+  res.redirect(`/server`);
+});
+app.get("/server", (req, res) => {
+  res.send("This is the server.");
+});
+
+if (process.env.PROD) {
+  app.use(express.static(path.join(__dirname, "./client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  });
+}
+
+const port = process.env.PORT || 5000;
+server.listen(port, () => console.log(`Server is running on port ${port}`));
