afadef92f2eb08756ee4c9c0b7733d00d8b8d9cb
Samyak Maharjan <samyakjyapu33127@gmail.com>
2021-06-03 10:26:16 +0545

    implement content shift with drawer

--- client/src/routes/Test.js
+++ client/src/routes/Test.js
@@ -10,6 +10,9 @@ import CancelIcon from "@material-ui/icons/Cancel";
 import SpeedDial from "@material-ui/lab/SpeedDial";
 import SpeedDialAction from "@material-ui/lab/SpeedDialAction";
 import TabPanel from "../components/TabPanel";
+import clsx from "clsx";
+
+const drawerHeight = "30vh";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -23,15 +26,14 @@ const useStyles = makeStyles((theme) => ({
   },
   drawer: {
     width: "100%",
-    height: "30vh",
     flexShrink: 0,
   },
   drawerPaper: {
     width: "100%",
-    height: "30vh",
+    height: drawerHeight,
     backgroundColor: theme.palette.secondary.light,
     position: "relative",
-    top: theme.spacing(0),
+    top: 0,
   },
   appBar: {
     zIndex: theme.zIndex.drawer + 1,
@@ -47,6 +49,22 @@ const useStyles = makeStyles((theme) => ({
       boxShadow: "none",
     },
   },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(0),
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginTop: `-${drawerHeight}`,
+  },
+  contentShift: {
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginTop: 0,
+  },
 }));
 
 const actions = [
@@ -59,12 +77,12 @@ const TestPage = () => {
   const theme = useTheme();
   const matches = useMediaQuery(theme.breakpoints.up("md"));
   const classes = useStyles();
-  const [open, setOpen] = useState(false);
+  const [openDrawer, setOpenDrawer] = useState(false);
   const [openSpeedDial, setOpenSpeedDial] = useState(false);
   const [value, setValue] = useState(0);
 
   const toggleDrawer = () => {
-    setOpen(!open);
+    setOpenDrawer(!openDrawer);
   };
 
   const tabClicked = (e, index) => {
@@ -98,9 +116,9 @@ const TestPage = () => {
             <IconButton
               color="inherit"
               onClick={toggleDrawer}
-              style={{ position: "absolute", top: "0.2rem", right: "3rem" }}
+              style={{ position: "absolute", top: "0.2rem", right: "3.5rem" }}
             >
-              {!open ? <LocalMoviesIcon /> : <CancelIcon />}
+              {!openDrawer ? <LocalMoviesIcon /> : <CancelIcon />}
             </IconButton>
 
             <SpeedDial
@@ -125,22 +143,28 @@ const TestPage = () => {
         </AppBar>
         <Drawer
           variant="persistent"
-          open={open}
+          open={openDrawer}
           anchor="top"
           className={classes.drawer}
           classes={{
             paper: classes.drawerPaper,
           }}
         ></Drawer>
-        <TabPanel value={value} index={0}>
-          Item One
-        </TabPanel>
-        <TabPanel value={value} index={1}>
-          Item Two
-        </TabPanel>
-        <TabPanel value={value} index={2}>
-          Item Three
-        </TabPanel>
+        <main
+          className={clsx(classes.content, {
+            [classes.contentShift]: openDrawer,
+          })}
+        >
+          <TabPanel value={value} index={0}>
+            Item One
+          </TabPanel>
+          <TabPanel value={value} index={1}>
+            Item Two
+          </TabPanel>
+          <TabPanel value={value} index={2}>
+            Item Three
+          </TabPanel>
+        </main>
       </div>
     );
   }
